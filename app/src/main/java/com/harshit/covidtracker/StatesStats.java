8db9ac62d3bef5417c323d1813c1a5a3f86803a6
carinoharshit <carinoharshit@gmail.com>
2020-11-29 05:24:10 +0530

    First Commit

---
+++ app/src/main/java/com/harshit/covidtracker/StatesStats.java
@@ -0,0 +1,9 @@
+package com.harshit.covidtracker;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface StatesStats {
+    @GET("covid19-in/stats/latest")
+    Call<BaseApiClassIndia> getStatesData();
+}
